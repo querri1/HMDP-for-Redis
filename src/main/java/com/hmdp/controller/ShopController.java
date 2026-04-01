a7bc6b6f4ce60ea5df71b820fa64e30067cfbc93
querri1 <204822173+querri1@users.noreply.github.com>
2026-04-01 20:19:28 +0800

    添加了商铺缓存

--- src/main/java/com/hmdp/controller/ShopController.java
+++ src/main/java/com/hmdp/controller/ShopController.java
@@ -21,7 +21,7 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping("/shop")
-public class ShopController {
+public class  ShopController {
 
     @Resource
     public IShopService shopService;
@@ -33,7 +33,7 @@ public class ShopController {
      */
     @GetMapping("/{id}")
     public Result queryShopById(@PathVariable("id") Long id) {
-        return Result.ok(shopService.getById(id));
+        return shopService.queryById(id);
     }
 
     /**
