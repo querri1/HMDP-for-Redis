f0fd54530849095186224b8dff2bd74abb0700a1
querri1 <204822173+querri1@users.noreply.github.com>
2026-04-06 19:26:49 +0800

    实现优惠券秒杀功能（后续需完成分布式锁）

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,6 +3,7 @@ package com.hmdp;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -24,10 +28,35 @@ public class HmDianPingApplicationTests {
     @Resource
     private ShopServiceImpl shopService;
 
-    @Test
-    public void testSaveShop() throws InterruptedException{
-        Shop shop = shopService.getById(1L);
+    @Resource
+    private RedisIdWorker redisIdWorker;
 
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    public void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                Long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
+
+    @Test
+    public void testSaveShop() throws InterruptedException {
+        Shop shop = shopService.getById(1L);
         cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
     }
 }
